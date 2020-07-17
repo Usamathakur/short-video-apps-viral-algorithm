db5f2ca3257f8f3927919c9982f9f806fd8dfb39
Usamathakur <51357843+Usamathakur@users.noreply.github.com>
2020-07-17 19:34:16 +0530

    Add files via upload

---
+++ SVVAlgo.java
@@ -0,0 +1,18 @@
+public class Main
+{
+	public static void main(String[] args) {
+		int i =0;
+		int top = 10;
+		
+		int current = top-1;
+		
+		for(i=0;i<top;i++){
+		    if(i<current){
+		        if(i<top){
+		            System.out.println("Video Virality " + i);
+		        
+		    }
+		    }
+		}
+	}
+}
